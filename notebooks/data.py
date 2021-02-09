7d4f9522a4728981e3be30ea1d172e0c70b3b402
Samir Azzouz <sazzouz@optimity.co.uk>
2021-02-09 13:55:15 +0000

    Preprocessing Florida Dataset
    Joining CTP and mobility datasets for FLorida specifically

---
+++ notebooks/data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def get_ctp():
+    return pd.read_csv('../datasets/ctp/ctp-all.csv')
+
+def get_mobility():
+    return pd.read_csv('../datasets/mobility/2020_US_Region_Mobility_Report.csv')
