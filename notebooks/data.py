1cac810ed25d521ec038dda27a03c36123f18fd9
Samir Azzouz <sazzouz@optimity.co.uk>
2021-02-12 17:45:03 +0000

    data preprocessing and scaling steps

--- notebooks/data.py
+++ notebooks/data.py
@@ -5,3 +5,6 @@ def get_ctp():
 
 def get_mobility():
     return pd.read_csv('../datasets/mobility/2020_US_Region_Mobility_Report.csv')
+
+def get_mask_usage():
+    return pd.read_csv('../datasets/nyt/mask-use.csv')
